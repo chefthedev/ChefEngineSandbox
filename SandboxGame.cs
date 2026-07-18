fae5cea5bfa5a31e5a91ce4912f58e63b53f4fbf
Adam Mattioli <amattiolidev@gmail.com>
2026-07-18 01:51:47 -0400

    tutorial/MonoGame2D breaking away from tutorial, using 12 as inspiration to create Player class and update SandboxGame to better represent usage of this class.

--- SandboxGame.cs
+++ SandboxGame.cs
@@ -2,7 +2,6 @@
 using ChefEngine.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace ChefEngineSandbox
 {
@@ -11,15 +10,12 @@ namespace ChefEngineSandbox
     /// </summary>
     public class SandboxGame : Engine
     {
-        // The slime and bat animated sprites.
-        private AnimatedSprite _slime;
-        private AnimatedSprite _bat;
+        // The player and bat animated sprites.
+        private AnimatedSprite _playerSprite;
+        private AnimatedSprite _batSprite;
 
-        // The slime's current position.
-        private Vector2 _slimePosition;
-
-        // Constant for the slime's movement speed.
-        private const float MOVEMENT_SPEED = 100.0f;
+        // The player.
+        private Player _player;
 
         /// <summary>
         /// Constructor for the SandboxGame class.
@@ -35,9 +31,12 @@ namespace ChefEngineSandbox
 
             base.Initialize();
 
-            // Adjust the properties of the slime and bat animated sprites.
-            _slime.Scale = new Vector2(4.0f);
-            _bat.Scale = new Vector2(4.0f);
+            // Adjust the properties of the player and bat animated sprites.
+            _playerSprite.Scale = new Vector2(4.0f);
+            _batSprite.Scale = new Vector2(4.0f);
+
+            // Initialize the player.
+            _player = new Player(new Vector2(0.0f), _playerSprite);
         }
 
         protected override void LoadContent()
@@ -45,63 +44,21 @@ namespace ChefEngineSandbox
             // Load the texture atlas from the xml config file.
             TextureAtlas atlas = TextureAtlas.CreateFromFile(Content, "images/atlas-definition.xml");
 
-            // Create the slime and bat animated sprite from the atlas.
-            _slime = atlas.CreateAnimatedSprite("slime-animation");
-            _bat = atlas.CreateAnimatedSprite("bat-animation");
+            // Create the player and bat animated sprite from the atlas.
+            _playerSprite = atlas.CreateAnimatedSprite("slime-animation");
+            _batSprite = atlas.CreateAnimatedSprite("bat-animation");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            // Update the slime and bat animated sprites.
-            _slime.Update(gameTime);
-            _bat.Update(gameTime);
+            // Update the player.
+            _player.Update(Input, gameTime);
 
-            // Check and handle keyboard input events.
-            CheckAndHandleKeyboardInput(gameTime);
+            // Update the bat animated sprite.
+            _batSprite.Update(gameTime);
 
             // Call the base class's Update method.
             base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// Checks the current keyboard state and moves the slime character.
-        /// </summary>
-        /// <param name="gameTime">The game time instance.</param>
-        private void CheckAndHandleKeyboardInput(GameTime gameTime)
-        {
-            // Calculate the adjusted movement speed with delta time.
-            float adjustedMovementSpeed = MOVEMENT_SPEED * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            // If the space bar is pressed.
-            if (Input.Keyboard.IsKeyDown(Keys.Space))
-            {
-                // Multiply the movement speed to apply a sprint mechanic.
-                adjustedMovementSpeed *= 5f;
-            }
-            // If the W key is pressed.
-            if (Input.Keyboard.IsKeyDown(Keys.W))
-            {
-                // Move the slime up.
-                _slimePosition.Y -= adjustedMovementSpeed;
-            }
-            // If the S key is pressed.
-            if (Input.Keyboard.IsKeyDown(Keys.S))
-            {
-                // Move the slime down.
-                _slimePosition.Y += adjustedMovementSpeed;
-            }
-            // If the A key is pressed.
-            if (Input.Keyboard.IsKeyDown(Keys.A))
-            {
-                // Move the slime left.
-                _slimePosition.X -= adjustedMovementSpeed;
-            }
-            // If the D key is pressed.
-            if (Input.Keyboard.IsKeyDown(Keys.D))
-            {
-                // Move the slime right.
-                _slimePosition.X += adjustedMovementSpeed;
-            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -112,9 +69,11 @@ namespace ChefEngineSandbox
             // Begin the sprite batch to prepare for 2D rendering with point sampling for sharp pixel art.
             SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
-            // Draw the slime and bat animated sprites.
-            _slime.Draw(SpriteBatch, _slimePosition);
-            _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+            // Draw the player.
+            _player.Draw(SpriteBatch);
+
+            // Draw the bat animated sprite.
+            _batSprite.Draw(SpriteBatch, new Vector2(50, 0));
 
             // End the sprite batch to finish 2D rendering.
             SpriteBatch.End();
